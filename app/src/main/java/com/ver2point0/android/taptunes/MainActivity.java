05a32ec1a9a7294b0fd53fc7ea6597e24ce7621b
John McIntosh II <jmci10@hotmail.com>
2017-02-28 03:17:16 -0500

    create view styles

--- app/src/main/java/com/ver2point0/android/taptunes/MainActivity.java
+++ app/src/main/java/com/ver2point0/android/taptunes/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ver2point0.android.taptunes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the album image
+        ImageView albumImage = (ImageView) findViewById(R.id.album_image);
+        // Set a click listener on the albumImage view
+        if (albumImage != null) {
+            albumImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent albumImageIntent = new Intent(MainActivity.this, TuneDetails.class);
+                    startActivity(albumImageIntent);
+                }
+            });
+        }
     }
 }
