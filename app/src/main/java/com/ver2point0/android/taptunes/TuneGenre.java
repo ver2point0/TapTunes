3d20e8a211f56324d775cdf38af578bb21dbb51a
John McIntosh II <jmci10@hotmail.com>
2017-02-28 05:55:56 -0500

    create genre view styles

--- app/src/main/java/com/ver2point0/android/taptunes/TuneGenre.java
+++ app/src/main/java/com/ver2point0/android/taptunes/TuneGenre.java
@@ -1,7 +1,10 @@
 package com.ver2point0.android.taptunes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class TuneGenre extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class TuneGenre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tune_genre);
+
+        // Find the View that shows the genre one linearlayout
+        LinearLayout genreLinear = (LinearLayout) findViewById(R.id.genre_one);
+        // Set a click listener on the Genre linearlayout
+        if (genreLinear != null) {
+            genreLinear.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent genreLinearIntent = new Intent(TuneGenre.this,  TuneDetails.class);
+                    startActivity(genreLinearIntent);
+                }
+            });
+        }
     }
 }
